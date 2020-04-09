8666867689dcb9b44af164b35b8f9ea8101dc6fb
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-04-09 13:57:27 +0100

    redesign workout card

--- workout-tracker/src/components/WorkoutCard/WorkoutCard.js
+++ workout-tracker/src/components/WorkoutCard/WorkoutCard.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Modal, Card, Button, Col, Icon, List } from 'antd';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Modal, Card, Col, Icon, List, Tooltip } from "antd";
 
 const { Meta } = Card;
 
@@ -9,105 +9,105 @@ class WorkoutCard extends React.Component {
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
   render() {
-  return (
-    <>
-    <Col>
-      <Card
-        style={{ width: "15.1rem", marginBottom: '1rem' }}
-        cover={<img alt="cover" src={this.props.image} style={{width: "15rem", height: "15rem", objectFit: "cover"}}/>}
-      >
-        <Meta
-          
-          title={this.props.name} style={{margin: '.5rem 0'}}
-          description={this.props.myWorkout
-            ? <ul style={{listStyle: 'none'}}>
-                <li>Difficulty: {this.props.difficulty}</li>
-              </ul>
-            : this.props.description
-          }
-          
-          />
-          <Button type="primary" size="small" block style={{fontWeight: 'bold'}}>
-            <Link onClick={this.props.startWorkout} to='/Workout_session'>Start Workout</Link>
-          </Button>
-          <div style={{display: 'flex', justifyContent: 'space-around', margin: '.5rem 1rem -1rem 1rem'}}>
-          {!this.props.myWorkout
-          ? <Icon
-              type="plus-circle"
-              onClick={this.props.addWorkout}
-            />
-          : <Icon
-              type="minus-circle"
-              onClick={this.props.deleteWorkout}
-            />
-          }
-        <Icon
-          type="info-circle"
-          onClick={this.showModal}/>
-
-          </div>
-      </Card>
-    </Col>
-    <Modal
-      title={this.props.name}
-      visible={this.state.visible}
-      onOk={this.handleOk}
-      onCancel={this.handleCancel}
-      >
-      <div style={{display: 'flex'}}>
-        <img alt="cover" src={this.props.image} style={{width: "15rem", height: "15rem", objectFit: "cover"}}/>
-        <p><span style={{fontWeight: 'bold'}}>Description: </span>{this.props.description}</p>
-      </div>
-      <div>
-        {this.props.exercises ? (
+    return (
+      <>
+        <Col>
+          <Card
+            cover={
+              <img
+                alt="cover"
+                src={this.props.image}
+                style={{ width: "100%", height: "15rem", objectFit: "cover" }}
+              />
+            }
+            title={this.props.name}
+            actions={[
+              <Tooltip placement="bottom" title="Start Workout">
+                <Link onClick={this.props.startWorkout} to="/Workout_session">
+                  <Icon
+                    style={{ fontSize: "16px" }}
+                    type="play-circle"
+                    key="play"
+                  />
+                </Link>
+              </Tooltip>,
+              !this.props.myWorkout ? (
+                <Tooltip placement="bottom" title="Add to My Workouts">
+                  <Icon type="plus-circle" onClick={this.props.addWorkout} />
+                </Tooltip>
+              ) : (
+                <Tooltip placement="bottom" title="Remove from My workouts">
+                  <Icon
+                    type="minus-circle"
+                    onClick={this.props.deleteWorkout}
+                  />
+                </Tooltip>
+              ),
+              <Tooltip placement="bottom" title="More info">
+                <Icon type="info-circle" onClick={this.showModal} />
+              </Tooltip>
+            ]}
+          >
+            <Meta description={this.props.description} />
+          </Card>
+        </Col>
+        <Modal
+          title={this.props.name}
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
           <div>
-            <List
-              size="small"
-              header={<h3>Exercises</h3>}
-              bordered
-              dataSource={this.props.exercises
-                .reduce((acc, current) => {
-                  const x = acc.find(
-                    item => item.exercise_name === current.exercise_name
-                  );
-                  if (!x) {
-                    return acc.concat([current]);
-                  } else {
-                    return acc;
-                  }
-                }, [])
-                .map(exercise => exercise.exercise_name)}
-              renderItem={item => (
-                <List.Item>
-                  {item}
-                </List.Item>
-              )}
-            />
+            <h4>Description: </h4>
+            <p>{this.props.description}</p>
           </div>
-        ) : null}
+          <div>
+            {console.log(this.props)}
+            {this.props.exercises ? (
+              <div>
+                <List
+                  size="small"
+                  header={<h3>Exercises</h3>}
+                  bordered
+                  dataSource={this.props.exercises
+                    .reduce((acc, current) => {
+                      const x = acc.find(
+                        item => item.exercise_name === current.exercise_name
+                      );
+                      if (!x) {
+                        return acc.concat([current]);
+                      } else {
+                        return acc;
+                      }
+                    }, [])
+                    .map(exercise => exercise.exercise_name)}
+                  renderItem={item => <List.Item>{item}</List.Item>}
+                />
               </div>
-    </Modal>
-  </>
-  )
+            ) : null}
+          </div>
+        </Modal>
+      </>
+    );
   }
 }
 export default WorkoutCard;
